ad9d0fbb86874e3f716497ee4abf32155946e786
Maksym Bondar <maksimbondar225@gmail.com>
2025-07-11 17:39:18 +0200

    feat: update logo

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Brain } from 'lucide-react';
 
 const Footer: React.FC = () => {
   return (
@@ -7,13 +6,16 @@ const Footer: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <div className="flex items-center justify-center mb-4">
-            <div className="bg-gradient-to-r from-blue-600 to-purple-600 p-2 rounded-xl">
-              <Brain className="w-5 md:w-6 h-5 md:h-6 text-white" />
-            </div>
+            <img
+              src='../public/logo.webp'
+              alt='Lexipath Logo'
+              className='size-12 rounded-2xl'
+            >
+            </img>
             <span className="ml-3 text-lg md:text-xl font-bold">Lexipath</span>
           </div>
           <p className="text-gray-400 mb-4 text-sm md:text-base">
-              Your path to better vocabulary 
+            Your path to better vocabulary
           </p>
           <p className="text-gray-500 text-xs md:text-sm">
             © 2025 Lexipath. Made with ❤️ for language learners.
